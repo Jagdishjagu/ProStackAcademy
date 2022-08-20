6b1cb582f969af51a05db0362d0638309e35ed84
Jagdishjagu <Jagujagdish199@gmail.com>
2022-08-20 09:27:08 -0700

    okie

---
+++ html/script/data.js
@@ -0,0 +1,22 @@
+function over() {
+    alert("Hello Good Morning");
+  }
+  
+  function out() {
+    alert("Hello Good Afternoon");
+  }
+  function click() {
+    alert("Hello Good Evening");
+  }
+  function ondblclick() {
+    alert("Hello Good Night");
+  }
+  
+  function onfocus() {
+    alert("Hello Early Morning");
+  }
+  
+  function onblur() {
+    alert("Hello late Night");
+  }
+  

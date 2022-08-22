035d655b20d6522e585b701ea908757faba4ca29
Jagdishjagu <Jagujagdish199@gmail.com>
2022-08-22 04:19:49 -0700

    okie

---
+++ html/javascript/conditionflow/if.js
@@ -0,0 +1,5 @@
+// if(condition){statement}//
+var name="Rahul Gandhi"
+if(name == "Rahul Gandhi"){
+    console.log("Future Prime minister")
+}
---
+++ html/javascript/operators/concatination.js
@@ -0,0 +1,16 @@
+// Multiplication//
+var a = 2*3
+console.log(a);
+var b = 2*"Hello"
+console.log(b);
+var c = 2*"2"
+console.log(c);
+
+//Addition//
+
+var d = 2+"2"
+console.log(d);
+var e = 2 + 2
+console.log(e);
+
+// Subraction//

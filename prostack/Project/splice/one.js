287ce71b221ebfa3ca696d1965a72c6c6d01efdd
Jagdishjagu <Jagujagdish199@gmail.com>
2022-10-11 00:42:57 -0700

    okie

---
+++ prostack/Project/splice/one.js
@@ -0,0 +1,5 @@
+//
+
+var cities = ["bangalore", "chennai", "goa", "hydrabad", "mumbai", "kolkata"];
+cities.splice(3, 2);
+console.log(cities);
---
+++ prostack/Project/spreadoperators/one.js
@@ -0,0 +1,6 @@
+let user = [
+  { id: 101, Name: "Rahul", Sal: 15000, Location: "Delhi" },
+  { id: 102, Name: "Sonia", Sal: 25000, Location: "Mumbai" },
+  { id: 103, Name: "Priyanka", Sal: 35000, Location: "Kolkata" },
+  { id: 104, Name: "Modi", Sal: 45000, Location: "Bangolore" },
+];
